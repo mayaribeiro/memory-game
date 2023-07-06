2bf94b085b4f319b7cdc800ccc4758b151beb317
Mayara Ribeiro <102433847+mayaribeiro@users.noreply.github.com>
2023-07-05 23:27:00 -0300

    Create login.js
    Criadas as funções que validam a entrada do nome do jogador e o salvam no Local Storage.

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('#player-name');
+const button = document.querySelector('#play');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({target}) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
+    button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = '../pages/game.html';
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
